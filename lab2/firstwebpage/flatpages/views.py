10024dbef009d9026b0c39dd3020ba5575280fe3
LordStrannik <81094780+LordStrannik@users.noreply.github.com>
2023-05-23 04:42:16 +0500

    lab2.3

--- lab2/firstwebpage/flatpages/views.py
+++ lab2/firstwebpage/flatpages/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 def home(request):
-	return render(request, 'index.html')
+	return render(request, 'static_handler.html')
 	# ~ return HttpResponse(u'Hello, World!', content_type="text/plain")
