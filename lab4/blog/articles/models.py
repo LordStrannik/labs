efb14a33a1c644579179d80a98681eebe7ca352c
LordStrannik <81094780+LordStrannik@users.noreply.github.com>
2023-05-23 11:51:32 +0500

    lab4. complete

--- lab4/blog/articles/models.py
+++ lab4/blog/articles/models.py
@@ -5,8 +5,14 @@ class Article(models.Model):
 	author = models.ForeignKey(User, on_delete=models.CASCADE)
 	text = models.TextField()
 	created_date = models.DateField(auto_now_add=True)
+	month_list = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня','июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря']
 
 	def __unicode__ (self):
 		return "%s: %s" % (self.author.username, self.title)
 	def get_excerpt(self):
 		return self.text[:140] + "..." if len(self.text) > 140 else self.text
+	def get_text(self):
+		return self.text
+	def get_date(self):
+		d = self.created_date
+		return "%d %s %d" % (d.day, self.month_list[d.month-1], d.year)
